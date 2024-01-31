35951faf5735be80c29d5dc17d9501e2de82b1ee
setepud <158361501+setepud@users.noreply.github.com>
2024-01-31 16:56:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["chlorine page"];
 
 
 let currentIndex = 0;
